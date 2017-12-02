b1a2272f3ccd2fa278d273f5e81eca5ec3773d5b
alexander-liao <alex.yj.liao@gmail.com>
2017-12-02 12:39:58 -0500

    christmas tree outline

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,7 +1,7 @@
-import sys, os, random, time
+import sys, os, fcntl, struct, termios, random, time
 
 def draw_char(r, c, char):
-    sys.stdout.write("\033[%d;%dH%s\033[1B" % (r, c, char ))
+    sys.stdout.write("\033[%d;%dH%s\033[1B" % (r, c, char))
 
 def write_char(char):
     sys.stdout.write(char)
@@ -13,15 +13,8 @@ def clear():
 def reset():
     sys.stdout.write("\033[0m\033[40m")
 
-def rows():
-    return os.get_terminal_size().lines
-
-def cols():
-    return os.get_terminal_size().columns
-
 def size():
-    size = os.get_terminal_size()
-    return (size.lines, size.columns)
+    return struct.unpack('HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)))[:2]
 
 def progress_snow(snow, grid):
     snow[:] = [(particle[0] + 1, particle[1]) for particle in snow]
@@ -36,17 +29,28 @@ def progress_snow(snow, grid):
 
 def draw_tree(grid):
     ROWS, COLS = size()
-    if COLS % 2 == 0:
-        grid[-1][COLS // 2] = "|"
-    else:
-        pass
+    center = COLS // 2
+    grid[-1][center - 1] = grid[-1][center + 1] = "\033[1;30m|\033[0m"
+    hbound = COLS - (COLS % 1) // 2
+    height = min(ROWS * 2 // 3, hbound * 2 // 3 + (hbound % 2)) - 1
+    grid[~height][center] = "\033[1;31m*\033[0m"
+    offset = 0
+    for i in range(1, height):
+        grid[~height + i][center + ~offset] = "\033[32m/\033[0m"
+        grid[~height + i][center - ~offset] = "\033[32m\\\033[0m"
+        if height - i % 3 != 1:
+            offset += 1
 
-snow = []
-while True:
-    clear()
-    grid = [[' '] * cols() for _ in range(rows())]
-    progress_snow(snow, grid)
-    draw_tree(grid)
-    sys.stdout.write("\n".join(map("".join, grid)))
-    sys.stdout.flush()
-    time.sleep(1)
+def main():
+    snow = []
+    for i in range(10):
+        clear()
+        ROWS, COLS = size()
+        grid = [[' '] * COLS for _ in range(ROWS)]
+        progress_snow(snow, grid)
+        draw_tree(grid)
+        sys.stdout.write("\n".join(map("".join, grid)))
+        sys.stdout.flush()
+        time.sleep(1)
+    return
+
